a599270a4230f3a90af8691884a3ddc80f9a0fce
JussiIH <48416422+JussiIH@users.noreply.github.com>
2019-03-24 20:59:44 +0200

    Update Kuningas.java

--- Shakki testailua/src/Kuningas.java
+++ Shakki testailua/src/Kuningas.java
@@ -5,8 +5,6 @@ public class Kuningas extends Nappula{
 	}
 	
 	public boolean Onkosiirtosallittu(int x, int y){
-		boolean a = true;
-		boolean b = false;
 		if (elossa == 1) {
 			if (y > 0 && y <= 8 && x > 0 && x <= 8) {
 				if( (x == this.sijaintiX && y == this.sijaintiY +1)
@@ -17,18 +15,18 @@ public class Kuningas extends Nappula{
 					|| (x == this.sijaintiX -1 && y == this.sijaintiY -1)
 					|| (x == this.sijaintiX +1&& y == this.sijaintiY -1)
 					|| (x == this.sijaintiX -1 && y == this.sijaintiY +1)){
-					return a;
+					return true;
 				}
 				else {
-					return b;
+					return false;
 				}
 			}
 			else {
-				return b;
+				return false;
 			}
 		}
 		else {
-			return b;
+			return false;
 		}
 	}
 }
